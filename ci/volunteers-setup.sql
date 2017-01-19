0b43e3925f54e6617a7c37ba7a05575dd41b34f8
andrewlarsen514 <andrew.larse514@gmail.com>
2017-01-18 23:14:10 -0600

    add db setup

---
+++ ci/volunteers-setup.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Volunteers ( volunteer_id INT NOT NULL AUTO_INCREMENT, volunteer_name VARCHAR(100) NOT NULL, created_date DATE, PRIMARY KEY (volunteer_id), UNIQUE (volunteer_name)
+
+);
+
+insert into tfcj_rds_db.Volunteers (volunteer_name , created_date ) values ("Alec Larsen", curdate());
+insert into tfcj_rds_db.Volunteers (volunteer_name , created_date ) values ("Andrew Larsen", curdate());
---
+++ test/routes/Volunteers_test.js
@@ -0,0 +1,4 @@
+var sinon = require('sinon'),
+    Issue = require('../model/issue'),
+    assert = require('chai').assert,
+    expect = require('chai').expect
